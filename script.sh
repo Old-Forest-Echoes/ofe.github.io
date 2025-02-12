32879a449f5f2231cb2ecb6b76ce1da316747fad
msand <msand@abo.fi>
2025-02-12 21:53:23 +0100

    More automation

--- script.sh
+++ script.sh
@@ -1,7 +1,13 @@
+#!/bin/bash
+set -e  # Exit on any error
+
+echo "Starting website sync..."
+
 # Clean up all subfolders in current directory
 rm -rf */
 
 # Download the website
+echo "Downloading website..."
 wget --recursive \
      --span-hosts \
      --convert-links \
@@ -11,6 +17,11 @@ wget --recursive \
      https://barbora-xu-af5e6eb293c41d4bdd0c2e8c4636.webflow.io/
 
 # Deploy
+echo "Committing changes..."
+git config --global user.name "GitHub Actions Bot"
+git config --global user.email "actions@github.com"
 git add .
-git commit -m "Update"
+git commit -m "Update from Webflow [skip ci]"
 git push
+
+echo "Sync completed successfully!"
