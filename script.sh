73545bffc4e242f08291cb0084b0e2912ac6c4bb
msand <msand@abo.fi>
2025-02-12 19:58:17 +0100

    Update

--- script.sh
+++ script.sh
@@ -1,6 +1,14 @@
+# Clean up all subfolders in current directory
+rm -rf */
+
 # Download the website with specific file types and convert links
 wget -r -k -E -nH \
      --convert-links \
      --span-hosts \
      --domains=barbora-xu-af5e6eb293c41d4bdd0c2e8c4636.webflow.io,cdn.prod.website-files.com \
      https://barbora-xu-af5e6eb293c41d4bdd0c2e8c4636.webflow.io/
+
+# Git operations
+git add .
+git commit -m "Update"
+git push
