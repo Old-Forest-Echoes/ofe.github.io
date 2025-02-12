ab6cd033f41478e4c809ed9ffb43dd33f45c21b9
msand <msand@abo.fi>
2025-02-12 20:16:45 +0100

    Update

--- script.sh
+++ script.sh
@@ -1,14 +1,16 @@
 # Clean up all subfolders in current directory
 rm -rf */
 
-# Download the website with specific file types and convert links
-wget -r -k -E -nH \
-     --convert-links \
+# Download the website
+wget --recursive \
      --span-hosts \
+     --convert-links \
+     --adjust-extension \
+     --no-host-directories \
      --domains=barbora-xu-af5e6eb293c41d4bdd0c2e8c4636.webflow.io,cdn.prod.website-files.com \
      https://barbora-xu-af5e6eb293c41d4bdd0c2e8c4636.webflow.io/
 
-# Git operations
+# Deploy
 git add .
 git commit -m "Update"
 git push
