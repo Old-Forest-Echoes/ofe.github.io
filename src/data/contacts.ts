64d3c46e663b676029c1fbd80d56e88208b2c010
Mikael Sand <msand@seaber.io>
2026-06-03 22:59:37 +0300

    updates

--- src/data/contacts.ts
+++ src/data/contacts.ts
@@ -1,12 +1,12 @@
 export interface ContactEntry {
   title: string;
-  name: string;
+  name?: string;
   nameLanguage?: string;
   email: string;
 }
 
 export const contacts: ContactEntry[] = [
-  { title: 'General Inquiries', name: 'Carly Markkanen', email: 'info@oldforestechoes.com' },
+  { title: 'General Inquiries', email: 'info@oldforestechoes.com' },
   { title: 'Director', name: 'Barbora Šilhánová', nameLanguage: 'cs', email: 'barbora@oldforestechoes.com' },
 ];
 
